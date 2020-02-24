7b29719e222efb2e65318802327a528550d60c66
ali bougarne <bougarne.ali@gmail.com>
2020-02-24 22:02:07 +0100

    implement roles Guard and roles decorator

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post, Body, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Tag } from './tag.entity';
 import { TagsService } from './tags.service';
 import { TagDto } from './dto/tagDto.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/shared/roles/roles.decorator';
+import { RolesGuard } from 'src/shared/roles/roles.guard';
 
 @Controller('tags')
 export class TagsController {
     constructor(private readonly tagsService: TagsService) { }
-
+    
+    @UseGuards(AuthGuard('jwt'),RolesGuard)
+	@Roles('ADMIN')
     @Get('all')
     async getAllTags(): Promise<TagDto[]> {
         let tags: Tag[] = await this.tagsService.getAllTags();
