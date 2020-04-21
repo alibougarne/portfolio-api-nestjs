026151b57cd91d38861723d30a9494106301f506
ali bougarne <bougarne.ali@gmail.com>
2020-04-21 23:32:52 +0100

    add all missed dependencies

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,9 +3,12 @@ import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { Category } from './category.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), MulterModule.register({
+    dest: './client/resources/categories',
+  })],
   controllers: [CategoriesController],
   providers: [CategoriesService]
 })
