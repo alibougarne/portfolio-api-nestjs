4b0b56a6cecb35e8c0af4b4aae30d1bfeaaa0958
ali bougarne <bougarne.ali@gmail.com>
2020-04-24 17:45:50 +0100

    add ssl cfg to type orm

--- ormconfig.js
+++ ormconfig.js
@@ -39,20 +39,20 @@ const database = isProd ? 'postgres' :'postgres';
 module.exports = {
   type: "postgres",
   url: "postgres://ttrnwlmeqzgcaz:0d8979d0f6bc6504cf51cbacfebaaa9f2c5f7b12d26157f48539900589ff35d7@ec2-3-91-139-25.compute-1.amazonaws.com:5432/d9fdc6fjd76tl3",
-  ssl: {
-    // DO NOT DO THIS
-    // set up your ca correctly to trust the connection
-    rejectUnauthorized: false
-  },
+  // ssl: {
+  //   // DO NOT DO THIS
+  //   // set up your ca correctly to trust the connection
+  //   rejectUnauthorized: false
+  // },
   // database: 'd9fdc6fjd76tl3',
   // host: 'ec2-3-91-139-25.compute-1.amazonaws.com',
   // port: 5432,
   // username: 'ttrnwlmeqzgcaz',
   // password: '0d8979d0f6bc6504cf51cbacfebaaa9f2c5f7b12d26157f48539900589ff35d7',
   // We need add the extra SSL to use heroku on localhost
-  // extra: {
-  //     ssl: true,
-  // },
+  extra: {
+      ssl: true,
+  },
   synchronize: false,
   logging: !isProd,
   entities: [
