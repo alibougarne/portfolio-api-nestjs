0575fa636373cf5afc102f4ec9c288674cdfcb6d
ali bougarne <bougarne.ali@gmail.com>
2020-04-24 16:30:57 +0100

    set ssl to true  in orm config file

--- ormconfig.js
+++ ormconfig.js
@@ -39,7 +39,7 @@ require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create(
 module.exports = {
   type: "postgres",
   url: "postgres://ttrnwlmeqzgcaz:0d8979d0f6bc6504cf51cbacfebaaa9f2c5f7b12d26157f48539900589ff35d7@ec2-3-91-139-25.compute-1.amazonaws.com:5432/d9fdc6fjd76tl3",
-  ssl: {
+  ssl: isProd || {
     // DO NOT DO THIS
     // set up your ca correctly to trust the connection
     rejectUnauthorized: false
