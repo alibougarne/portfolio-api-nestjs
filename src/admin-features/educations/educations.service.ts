95a21ae3e5a03ceab57dacb40026df271a69d9f8
ali bougarne <bougarne.ali@gmail.com>
2020-05-15 22:42:22 +0100

    add delete education service

--- src/admin-features/educations/educations.service.ts
+++ src/admin-features/educations/educations.service.ts
@@ -2,7 +2,7 @@ import { Injectable, HttpStatus } from '@nestjs/common';
 import { Education } from './education.entity';
 import { CustomException } from 'src/app/exception/custom.exception';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DeleteResult } from 'typeorm';
 
 @Injectable()
 export class EducationsService {
@@ -25,6 +25,14 @@ export class EducationsService {
             return this.educationRepository.save(education);
         } catch (error) {
             throw new CustomException("Error creating Education",HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    async deleteEducation(educationId: string): Promise<DeleteResult> {
+        try {
+            return await this.educationRepository.delete(educationId);
+        } catch (error) {
+            throw new CustomException("Error deleting education",HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 }
