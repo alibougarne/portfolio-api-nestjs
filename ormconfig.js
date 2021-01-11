a6ce75f777a2d0675e5a3c5b846c266fa2294dcb
alibougarne <bougarne.ali@gmail.com>
2021-01-11 19:31:28 +0100

    remove dropDatabase from orm config

--- ormconfig.js
+++ ormconfig.js
@@ -14,7 +14,7 @@ const database = isProd ? 'postgres' : 'postgres';
 let connectionOptions = {
   type: 'postgres',
   // url: "postgres://ttrnwlmeqzgcaz:0d8979d0f6bc6504cf51cbacfebaaa9f2c5f7b12d26157f48539900589ff35d7@ec2-3-91-139-25.compute-1.amazonaws.com:5432/d9fdc6fjd76tl3",
-  dropSchema: isProd,
+  // dropSchema: isProd,
   synchronize: true,
   // autoLoadEntities: true,
   logging: !isProd,
