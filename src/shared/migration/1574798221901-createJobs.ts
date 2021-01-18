50cc70b4e31474f9fcbfc87a92d4b4182dcf3866
alibougarne <bougarne.ali@gmail.com>
2021-01-18 19:29:34 +0100

    adjust jobs  migration ==> Database migration

--- src/shared/migration/1574798221901-createJobs.ts
+++ src/shared/migration/1574798221901-createJobs.ts
@@ -2,29 +2,12 @@ import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
 import { Country } from '../../admin-features/countries/country.entity';
 import { Job } from '../../admin-features/jobs/job.entity';
 import { Company } from '../../admin-features/companies/company.entity';
-
+import jobs from './data/jobs.json'
 export class createJobs1574798221901 implements MigrationInterface {
   private jobRepository = getRepository(Job);
   private countryRepository = getRepository(Country);
   private companyRepository = getRepository(Company);
   public async up(queryRunner: QueryRunner): Promise<any> {
-    let jobs=[{
-      jobTitle : "Web developer",
-      mission : "create and maintain web sites",
-      company : "Dypix",
-      country : "Algeria",
-      beginDate:new Date(2014, 12, 1),
-      endDate:new Date(2016,12, 1),
-    },
-    {
-      jobTitle : "Software Enginner",
-      mission : "create and maintain applications for employees of Algeria airways",
-      company : "Air Algérie",
-      country : "Algeria",
-      beginDate:new Date(2016,12, 13),
-      endDate:new Date(2018,1, 14),
-    }] 
-
     try {
       for (let job of jobs) {
         await this.jobRepository.save({
@@ -38,7 +21,7 @@ export class createJobs1574798221901 implements MigrationInterface {
         });
       }
     } catch (error) {
-      throw error;
+      console.error('%c⧭ error jobs migrations ===> ', 'color: #408059', error);
     }
   }
   public async down(queryRunner: QueryRunner): Promise<any> {}
